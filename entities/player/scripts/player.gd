029e13ecfbb2d3b2b691d758a9cf9e18a9aab5e3
gandar <davidezenobigarcia@gmail.com>
2025-02-07 01:03:08 +0100

    player related and small stuff

--- entities/player/scripts/player.gd
+++ entities/player/scripts/player.gd
@@ -6,29 +6,52 @@ signal ammo_changed;
 @export var max_health: int = 100;
 @export var speed: int = 800;
 @export var current_bullet: Enums.PlayerBullets;
-@export var fire_rate: float = 0.08;
+@export var fire_rate: float = 0.1;
 @export var max_ammo: int = 30;
 
 var time_since_last_shoot: float = 0.0;
 var current_ammo: int;
 
+var screen_size: Vector2;
+
 @onready var bullet_manager: Marker2D = $BulletManager;
 @onready var reload_cooldown: Timer = $ReloadCooldown;
 @onready var reload: Timer = $Reload;
 
-func _physics_process(delta):
+func _ready() -> void:
+	look_at(Vector2.UP);
+	screen_size = get_viewport_rect().size;
+	current_ammo = max_ammo;
+
+func _physics_process(delta) -> void:
+	## Look at / rotation
 	var mouse_position = get_global_mouse_position();
 	look_at(mouse_position);
 	
+	## Direction and movement
 	var new_direction = get_movement_input();
 	velocity = Vector2(new_direction.x * speed, new_direction.y * speed);
 	move_and_slide();
 	
-	#position.x = clamp(position.x, 0 + 50, screen_size.x - 50);
-	#position.y = clamp(position.y, 0 + 50, screen_size.y - 50);
+	## Dash TODO
+	
+	## Screen limits
+	position.x = clamp(position.x, 0 + 50, screen_size.x - 50);
+	position.y = clamp(position.y, 0 + 50, screen_size.y - 50);
+	
+	## Shoot
+	if Input.is_action_pressed("shoot") and time_since_last_shoot <= 0 and current_ammo > 0:
+		shoot();
+	
+	if time_since_last_shoot > 0:
+		time_since_last_shoot = clamp(time_since_last_shoot - delta, 0, fire_rate);
 
-func shoot():
+func shoot() -> void:
 	bullet_manager.spawn_bullet(current_bullet, position, rotation);
+	time_since_last_shoot = fire_rate;
+	current_ammo -= 1;
+	reload_cooldown.start();
+	reload.stop();
 
 func get_movement_input() -> Vector2:
 	var direction = Vector2.ZERO;
@@ -43,3 +66,12 @@ func get_movement_input() -> Vector2:
 	
 	direction = direction.normalized();
 	return direction;
+
+func _start_reloading() -> void:
+	reload.start();
+
+func _reload_ammo() -> void:
+	current_ammo += 1;
+	if current_ammo == max_ammo:
+		reload.stop();
+	ammo_changed.emit();
