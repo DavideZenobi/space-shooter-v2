742c68fc5667b0c7336abc05bad8f09245b33a83
gandar <davidezenobigarcia@gmail.com>
2025-02-15 00:11:47 +0100

    2 lines

--- singletons/global_variables.gd
+++ singletons/global_variables.gd
@@ -1,4 +1,7 @@
 extends Node
 
-var game_speed: int = 1;
+var game_speed: float = 1.0;
 var is_game_freezed: bool = false;
+
+func set_game_speed(new_game_speed: float):
+	game_speed = new_game_speed;
